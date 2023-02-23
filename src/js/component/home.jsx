2ae9b86566e8d12cb8d3181a6e610f10c2b7648c
achapdesign <achapdesign@gmail.com>
2023-02-23 23:30:00 +0000

    1more

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,22 +2,30 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 //create your first component
-const Home = () => {  
-const [color, setColor] = useState("red");
-//this.state.first = ["red", ]
-//  useEffect(() => {
-//	if (setColor)
-    document.title = `${color} light!`;
- // },[color]);
+const Home = () => {
+  const [color, setColor] = useState("red");
+  //this.state.first = ["red", ]
+  //  useEffect(() => {
+  //	if (setColor)
+  document.title = `${color} light!`;
+  // },[color]);
 
   return (
     <div>
-		<div className="traffic-light" >
-			<div className="red" shadowSize={2}><i class="fa-solid fa-circle"></i></div>
-			<div className="yellow">yellow light</div>
-			<div className="green">green light</div>
-    	</div>
-	</div>
+      <div className="traffic-light">
+        <div className="container">
+          <div className="red">
+            <i class="fa-solid fa-circle"></i>
+          </div>
+          <div className="yellow">
+            <i class="fa-solid fa-circle"></i>
+          </div>
+          <div className="green">
+            <i class="fa-solid fa-circle"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
