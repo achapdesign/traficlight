79e3fd8f6eb027caf847838ce7fcf03984a4b2ae
achapdesign <achapdesign@gmail.com>
2023-02-27 00:06:40 +0000

    used some help

--- src/js/component/trafficlight.js
+++ src/js/component/trafficlight.js
@@ -1,7 +1,38 @@
 import React from 'react'
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function trafficlight() {
+    // ... Your code
+    
+    useEffect(() => {
+      const intervalHandle = setInterval( /** Your callback code */ )
+      
+      // This will cancel the previous timer each time the component is re-rendered due to changing any of 
+      // the passed dependencies.
+      return function cleanup() {
+        clearInterval(intervalHandle)
+      }
+    }, [red, green, yellow])
+  
+    // Your code
+  }
+  
+  
+function TrafficLight() {
+    // ... Your code
+    
+    useEffect(() => {
+      const intervalHandle = setInterval( /** Your callback code */ )
+      
+      // This will cancel the timer after the component unmounts.
+      return function cleanup() {
+        clearInterval(intervalHandle)
+      }
+    }, [])
+  
+    // Your code
+  }
+  
     const [color, setColor] = useState("red");
     document.title = `${color} light!`;
   return (
